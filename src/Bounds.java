da96924f10d0ccef1e415d98c3f5708efaba011e
drjekyll3 <vidya3v@gmail.com>
2014-01-30 23:37:41 -0800

    Added answers from a run.

--- src/Bounds.java
+++ src/Bounds.java
@@ -52,7 +52,6 @@ public class Bounds {
 		}
 		Bounds bounds = new Bounds();
 		System.out.println(bounds.stringForm(numbers));
-		
 		bounds.findBounds(numbers, 11);
 		bounds.findBounds(numbers, 4);
 		bounds.findBounds(numbers, 9);
@@ -61,6 +60,33 @@ public class Bounds {
 		bounds.findBounds(numbers, 23);
 		bounds.findBounds(numbers, 98);
 		bounds.findBounds(numbers, 8);
+		
+		
+/*		[40, 14, 25, 25, 29, 17, 13, 44, 36, 5, 21, 41, 6, 19, 2, 16, 19, 19, 40, 42, 47, 32, 30, 4, 10, 27, 41, 7, 38, 13, 3, 43, 8, 9, 37, 19, 18, 6, 13, 11, 20, 1, 10, 33, 47, 20, 37, 31, 6, 18, 6, 36, 14, 20, 3, 2, 30, 12, 15, 28, 3, 28, 47, 4, 18, 11, 43, 25, 8, 32, 21, 31, 30, 2, 25, 27, 24, 5, 46, 5, 31, 35, 3, 32, 46, 19, 3, 2, 1, 44, 24, 12, 15, 7, 28, 27, 16, 31, 41, 12]
+		The lower bound of 11 is 39.
+		The upper bound of 11 is 65.
+
+		The lower bound of 4 is 23.
+		The upper bound of 4 is 63.
+
+		The lower bound of 9 is 33.
+		The upper bound of 9 is 33.
+
+		The lower bound of 50 is -1.
+		The upper bound of 50 is -1.
+
+		The lower bound of 34 is -1.
+		The upper bound of 34 is -1.
+
+		The lower bound of 23 is -1.
+		The upper bound of 23 is -1.
+
+		The lower bound of 98 is -1.
+		The upper bound of 98 is -1.
+
+		The lower bound of 8 is 32.
+		The upper bound of 8 is 68.
+*/	
 	}
 
 }
